ce344e8b104d82869801be132fb6d81779a54fd2
Bernardo Kanekiyo <bmsk.eng17@uea.edu.br>
2024-02-19 20:47:11 -0400

    Status do botao verificar

--- lib/Pages/doacao/minhasDoacoes_page.dart
+++ lib/Pages/doacao/minhasDoacoes_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_maps/Models/doacao_model.dart';
+import 'package:flutter_maps/Pages/doacao/statusMinhasDoacoes_page.dart';
 
 class MinhasDoacoesPage extends StatefulWidget {
   @override
@@ -38,6 +40,17 @@ class _MinhasDoacoesPageState extends State<MinhasDoacoesPage> {
           final doacoes = snapshot.data!.docs.map((doc) {
             final data = doc.data() as Map<String, dynamic>;
             bool emAndamento = data['status'] == 'EM ANDAMENTO';
+            // Criando uma instância de DoacaoModel e preenchendo-a com os dados
+            DoacaoModel doacao = DoacaoModel(
+              idDoacao: data['id_doacao'].toString(),
+              descricao: data['descricao'],
+              endereco: data['endereco'],
+              emailDoador: data['email_doador'],
+              emailReceptor: data['email_receptor'],
+              imageUrl: data['imagemUrl'],
+              status: data['status'],
+              // Você pode adicionar mais campos conforme necessário
+            );
             return Card(
               elevation: 3,
               margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
@@ -71,8 +84,15 @@ class _MinhasDoacoesPageState extends State<MinhasDoacoesPage> {
                 trailing: emAndamento
                     ? ElevatedButton(
                         onPressed: () {
-                          // Ação a ser realizada quando o botão for pressionado
-                          print('Botão "Verificar" pressionado!');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StatusMinhasDoacoesPage(
+                                doacao: doacao,
+                                idDocumento: doc.id,
+                              ),
+                            ),
+                          );
                         },
                         child: Text('Verificar'),
                       )
@@ -80,8 +100,7 @@ class _MinhasDoacoesPageState extends State<MinhasDoacoesPage> {
                         onPressed: null,
                         child: Text('Verificar'),
                         style: ElevatedButton.styleFrom(
-                          primary: Colors
-                              .grey, // Cor de fundo cinza quando desativado
+                          primary: Colors.grey,
                         ),
                       ),
               ),
